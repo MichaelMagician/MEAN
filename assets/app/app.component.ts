e0bd7c39bb39095a63beae5388607ef47fe004a2
Michael <Mguo1006@gmail.com>
2017-05-21 16:33:12 -0400

    change components

--- assets/app/app.component.ts
+++ assets/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-app',
-    templateUrl: './app.component.html'
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+ styles:['.author{display: inline-block} .config{display:inline-block} ']
+ 
+
 })
 export class AppComponent {
-    
+  message = {
+    content: "a message",
+    author: 'Meee'
+  };
 }
